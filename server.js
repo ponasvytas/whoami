3d0d1e4fbe92bd18f0f613cdcf3af74ada6a60d2
Vytautas Usaitis <ponasvytas@yahoo.com>
2016-10-23 22:53:57 +0000

    functional app

--- server.js
+++ server.js
@@ -1,2 +1,43 @@
 var express = require("express");
 var app = express();
+var locale = require("locale");
+var requestIP = require("request-ip");
+const os = require("os");
+
+var port = process.env.PORT || 8080
+
+app.use(requestIP.mw())
+app.use (locale())
+
+app.get("/", function (req, res){
+    
+    
+    var ip = req.clientIp;
+    var language = req.headers["accept-language"].substr(0,5);
+    var opSys = os.type();
+    var osRelease = os.release();
+    
+    
+    var obj = {
+      IP_address:ip,
+      language: language,
+      System: opSys + " " + osRelease+"; "+os.platform() + "; " + os.arch()
+  }
+    
+    res.send(JSON.stringify(obj));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
